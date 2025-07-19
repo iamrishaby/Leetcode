fefdd073606af685ab6406be15bbc5008e26b4f1
Rishab Yadav <115487678+iamrishaby@users.noreply.github.com>
2025-07-19 21:16:48 +0530

    Time: 51 ms (75.05%), Space: 48.9 MB (76.56%) - LeetHub

--- 1350-remove-sub-folders-from-the-filesystem/1350-remove-sub-folders-from-the-filesystem.cpp
+++ 1350-remove-sub-folders-from-the-filesystem/1350-remove-sub-folders-from-the-filesystem.cpp
@@ -1,22 +1,18 @@
+#include <vector>
+#include <string>
+#include <algorithm>
+
 class Solution {
 public:
-    vector<string> removeSubfolders(vector<string>& folder) {
-        sort(begin(folder), end(folder));
-        vector<string> result;
+    std::vector<std::string> removeSubfolders(std::vector<std::string>& folder) {
+        std::sort(folder.begin(), folder.end());
+        std::vector<std::string> ans = {folder[0]};
 
-        result.push_back(folder[0]);
-
-        for(int i = 1; i<folder.size(); i++){
-            string currfolder = folder[i];
-            string lastfolder = result.back();
-            lastfolder += '/';
-
-            if(currfolder.find(lastfolder)!= 0){
-                result.push_back(currfolder);
+        for (int i = 1; i < folder.size(); ++i) {
+            if (folder[i].substr(0, ans.back().size() + 1) != ans.back() + "/") {
+                ans.push_back(folder[i]);
             }
         }
-
-        return result;
-
+        return ans;
     }
 };
