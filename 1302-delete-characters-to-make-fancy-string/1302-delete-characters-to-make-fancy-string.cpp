630ead48eac5860dcc8178fbc24718c49df26fe9
Rishab Yadav <115487678+iamrishaby@users.noreply.github.com>
2025-07-21 21:36:48 +0530

    Time: 26 ms (64.27%), Space: 44.6 MB (13.57%) - LeetHub

--- 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.cpp
+++ 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.cpp
@@ -3,18 +3,15 @@ public:
     string makeFancyString(string s) {
         string result;
 
-        for (char c : s) {
-            int n = result.size();
-        
-        
-            if (n >= 2 && result[n - 1] == c && result[n - 2] == c) {
-                continue; 
-            }
-
-        
-            result += c;
+    for (char c : s) {
+        int n = result.size();
+        if (n >= 2 && result[n - 1] == result[n - 2] && result[n - 1] == c) {
+            continue; // Skip character if it makes 3 in a row
         }
+        result += c;
+    }
 
-        return result;
+    return result;
+        
     }
 };
